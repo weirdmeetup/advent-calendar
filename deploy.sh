c164e9cbc84dba0922700488a835c26970c55b53
Shia <rise.shia@gmail.com>
2016-11-10 20:22:49 +0900

    Add travis configuration for build asset

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Save some useful information
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+SHA=`git rev-parse --verify HEAD`
+
+git checkout -b gh-pages
+npm run build
+git add js -f && git commit -n -m "deploy to gh-pages ${SHA}"
+
+# Get the deploy key by using Travis's stored variables to decrypt deploy_key.enc
+ENCRYPTED_KEY_VAR="encrypted_${ENCRYPTION_LABEL}_key"
+ENCRYPTED_IV_VAR="encrypted_${ENCRYPTION_LABEL}_iv"
+ENCRYPTED_KEY=${!ENCRYPTED_KEY_VAR}
+ENCRYPTED_IV=${!ENCRYPTED_IV_VAR}
+openssl aes-256-cbc -K $ENCRYPTED_KEY -iv $ENCRYPTED_IV -in deploy_key.enc -out deploy_key -d
+chmod 600 deploy_key
+eval `ssh-agent -s`
+ssh-add deploy_key
+
+git push $SSH_REPO gh-pages -f 
