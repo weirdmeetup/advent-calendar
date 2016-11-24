895ecbcc01eddb474e05b9ce1141620979143e71
Shia <rise.shia@gmail.com>
2016-11-24 21:45:01 +0900

    Webhook test

--- notify.sh
+++ notify.sh
@@ -1,1 +1,1 @@
-curl -X POST --data-urlencode 'payload={"username": "Travis", "text": "Deployment is done successfully. you can check at http://1225.weirdx.io", "icon_emoji": ":octocat:"}' $SLACK_WEBHOOK_URL
+curl -X POST --data-urlencode 'payload={"username": "Travis", "text": "Deployment is done successfully. you can check at http://1225.weirdx.io", "icon_emoji": ":octocat:"}' $SLACK_WEBHOOK
